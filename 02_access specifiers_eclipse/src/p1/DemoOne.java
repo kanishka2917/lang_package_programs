c04648da685c64f4a865a6dc80ad5315dfc76e6c
Kanishka <kanishkab2917@gmail.com>
2024-06-04 10:04:38 +0530

    access specifiers

--- 02_access specifiers_eclipse/src/p1/DemoOne.java
+++ 02_access specifiers_eclipse/src/p1/DemoOne.java
@@ -1,5 +1,12 @@
 package p1;
 
 public class DemoOne {
-
+	public static void main(String args[]) {
+		
+		Hello hello = null;
+		hello = new HelloImpl();
+		hello.m1();
+		int res = hello.add(10, 20);
+		System.out.println("sum of both no is " + res);
+	}
 }
