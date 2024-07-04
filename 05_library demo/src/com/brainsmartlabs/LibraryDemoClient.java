a8d8d71c796a445713c27fcd8aa9cb2f90d7c3bc
Kanishka <kanishkab2917@gmail.com>
2024-07-04 17:22:48 +0530

    05_library understanding java libraries

---
+++ 05_library demo/src/com/brainsmartlabs/LibraryDemoClient.java
@@ -0,0 +1,12 @@
+package com.brainsmartlabs;
+
+public class LibraryDemoClient {
+	public static void main(String[] args) {
+		
+		String s1 = new String("hello");
+		String s2 = new String("hello");
+		boolean b = s1.equals(s2);
+		System.out.println(b);
+	}
+
+}
