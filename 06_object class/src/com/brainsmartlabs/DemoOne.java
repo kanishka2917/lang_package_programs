52475910af0dc066a7baaeb199467415a769226d
Kanishka <kanishkab2917@gmail.com>
2024-07-06 16:38:36 +0530

    object class

---
+++ 06_object class/src/com/brainsmartlabs/DemoOne.java
@@ -0,0 +1,38 @@
+package com.brainsmartlabs.object.getclass;
+
+class Object{
+	
+	class getlass();
+}
+class Box{
+	int w, h, d;
+	Box(int w, int h, int d){
+		
+	}
+	int vol() {
+		
+	}
+}
+	class Test{
+		int x;
+		int y;
+		Test(int x, int y){
+			
+		}
+		void m1() {
+			
+		}
+	}
+
+
+public class DemoOne {
+	public static void main(String[] args) {
+		Box b1 = new Box(1,2,3);
+		Test t1 = new Test(5,6);
+		Box b2 = new Box(7,8,9);
+		Test t2 = new Test(10,10);
+		class c1 = b1.getClass();
+		
+	}
+
+}
