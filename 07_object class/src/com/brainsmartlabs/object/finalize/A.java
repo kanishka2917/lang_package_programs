7d3e4f36221d7e1470bb015dca78e366e6c80223
Kanishka <kanishkab2917@gmail.com>
2024-07-06 17:23:32 +0530

    completing 07_object class

--- 07_object class/src/com/brainsmartlabs/object/finalize/A.java
+++ 07_object class/src/com/brainsmartlabs/object/finalize/A.java
@@ -2,6 +2,15 @@ package com.brainsmartlabs.object.finalize;
 
 public class A {
 	A() {
-		System.out.println();
+		System.out.println("initialising resources");
+	}
+	void m1() {
+		System.out.println("using resources in m1()");
+		}
+	void m2() {
+		System.out.println("using resources in m2()");
+		}
+	public void finalize() {
+		System.out.println("clearing the resourse");
 	}
 }
--- 07_object class/src/com/brainsmartlabs/object/finalize/FinalizeDemo.java
+++ 07_object class/src/com/brainsmartlabs/object/finalize/FinalizeDemo.java
@@ -1,7 +1,22 @@
 package com.brainsmartlabs.object.finalize;
+
 public class FinalizeDemo {
 	public static void main(String[] args) {
-		
-	} 
+
+		A a1 = new A();
+		A a2 = new A();
+		A a3 = new A();
+		A a4 = new A();
+		a1.m1();
+		a2.m1();
+		a1.m2();
+		a2.m2();
+		System.gc();
+		a1 = null;
+		a2 = null;
+		a3 = a4;
+		System.gc();
+
+	}
 
 }
