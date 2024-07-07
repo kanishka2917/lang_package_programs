7c0f67a2cb88ed753d130ae40f83c648687c621f
Kanishka <kanishkab2917@gmail.com>
2024-07-07 11:14:42 +0530

    before overriding to string

---
+++ 09_to_string/src/com/bsl/object/tostring/Demo.java
@@ -0,0 +1,12 @@
+package com.bsl.object.tostring;
+
+public class Demo {
+	
+	public static void main(String[] args) {
+		
+		Hello h = new Hello(10,20);
+		System.out.println(h);
+		System.out.println(h.toString());
+	}
+
+}
---
+++ 09_to_string/src/com/bsl/object/tostring/Hello.java
@@ -0,0 +1,16 @@
+package com.bsl.object.tostring;
+
+public class Hello {
+	
+	int i,j;
+
+	public Hello(int i, int j) {
+		super();
+		this.i = i;
+		this.j = j;
+	}
+	
+	//overriding to string method
+	
+
+}
