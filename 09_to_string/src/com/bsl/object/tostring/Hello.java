b3d02c62de228efb11f9acdb28716cd6b142f072
Kanishka <kanishkab2917@gmail.com>
2024-07-07 11:19:18 +0530

    after overriding to string

--- 09_to_string/src/com/bsl/object/tostring/Hello.java
+++ 09_to_string/src/com/bsl/object/tostring/Hello.java
@@ -1,16 +1,22 @@
 package com.bsl.object.tostring;
 
 public class Hello {
-	
-	int i,j;
+
+	int i, j;
 
 	public Hello(int i, int j) {
 		super();
 		this.i = i;
 		this.j = j;
 	}
-	
-	//overriding to string method
-	
+
+	// overriding to string method
+	@Override
+	public String toString() {
+
+		String s1 = "value of i and j is " + i + " & " + j ;
+		return s1;
+
+	}
 
 }
