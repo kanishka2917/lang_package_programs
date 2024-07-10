513fa22b461af73cd9e16c6b62a688ae4dd54e8a
Kanishka <kanishkab2917@gmail.com>
2024-07-10 19:18:10 +0530

    worked on substrings

--- 14_length/src/com/bml/string/object/length/StringDemo.java
+++ 14_length/src/com/bml/string/object/length/StringDemo.java
@@ -1,5 +1,15 @@
 package com.bml.string.object.length;
 
 public class StringDemo {
+	public static void main(String[] args) {
+		
+		String s1 = "apple";
+		System.out.println(s1);
+		String subs1 = s1.substring(2,4);
+		System.out.println(s1);
+		System.out.println(subs1);
+		System.out.println(s1.substring(2));
+
+	}
 
 }
