61d10585b7799ea03ef7c0949c5ca77bf3839fdd
Kanishka <kanishkab2917@gmail.com>
2024-07-10 19:09:41 +0530

    program using + symbol concatination

---
+++ 13_concat/src/com/bls/object/string/concat/Demo.java
@@ -0,0 +1,14 @@
+package com.bls.object.string.concat;
+
+public class Demo {
+	public static void main(String[] args) {
+		String s1 = "a";
+		String s2 = s1 + "b";
+		System.out.println(s1);
+		System.out.println(s2);
+		s1 = s1 + "c";
+		System.out.println(s1);
+		System.out.println(s1);
+	}
+
+}
