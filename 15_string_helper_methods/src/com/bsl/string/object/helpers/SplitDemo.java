c3b01a3c3284e1921c10a65bd55a1819b8006b31
Kanishka <kanishkab2917@gmail.com>
2024-07-10 19:41:33 +0530

    learnt split program

--- 15_string_helper_methods/src/com/bsl/string/object/helpers/SplitDemo.java
+++ 15_string_helper_methods/src/com/bsl/string/object/helpers/SplitDemo.java
@@ -3,7 +3,7 @@ package com.bsl.string.object.helpers;
 public class SplitDemo {
 
 	public static void main(String[] args) {
-			
+		
 		String s3 = "hello guys, how are you, thankyou very much.";
 		String []as3 = s3.split(" ");//space is the de-limmiter 
 		for(int i = 0; i<as3.length; i++) {
