c34bd9e7eaf89e739f36b6d18a83773ad249d3a1
Kanishka <kanishkab2917@gmail.com>
2024-07-10 19:47:45 +0530

    replace program in java

--- 15_string_helper_methods/src/com/bsl/string/object/helpers/ReplaceDemo.java
+++ 15_string_helper_methods/src/com/bsl/string/object/helpers/ReplaceDemo.java
@@ -3,8 +3,14 @@ package com.bsl.string.object.helpers;
 public class ReplaceDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		String s1 ="apple";
+		System.out.println(s1);
+		String subs1 = s1.substring(2,4);
+		System.out.println(s1);
+		System.out.println(subs1);
+		System.out.println(s1.substring(2));
+		
 	}
 
 }
