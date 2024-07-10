c4c110fb5dd0536ed92183a220b354d8ecb7e8fd
Kanishka <kanishkab2917@gmail.com>
2024-07-10 19:06:17 +0530

    program using concat method

--- 13_concat/src/com/bls/object/string/concat/StringConcatDemo.java
+++ 13_concat/src/com/bls/object/string/concat/StringConcatDemo.java
@@ -1,5 +1,15 @@
 package com.bls.object.string.concat;
 
 public class StringConcatDemo {
+	
+	public static void main(String[] args) {
+		
+		String s1 = "a";
+		String s2 = s1.concat("b");
+		System.out.println(s1);
+		System.out.println(s2);
+		s1 = s1.concat("c");
+		System.out.println(s1);		
+	}
 
 }
