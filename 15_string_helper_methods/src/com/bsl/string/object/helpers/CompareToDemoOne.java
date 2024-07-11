a95b45a025eec9e563990075b8e3a0b74da42080
Kanishka <kanishkab2917@gmail.com>
2024-07-11 09:53:40 +0530

    learning compareTo was  very easy

--- 15_string_helper_methods/src/com/bsl/string/object/helpers/CompareToDemoOne.java
+++ 15_string_helper_methods/src/com/bsl/string/object/helpers/CompareToDemoOne.java
@@ -3,8 +3,11 @@ package com.bsl.string.object.helpers;
 public class CompareToDemoOne {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		String s1 ="Hello";
+		String s2 = "hi";
+		int z = s1.compareTo(s2);
+		System.out.println(z);
 	}
 
 }
