c774fe2c577c917b14dc8fff59f044903e6d8acf
Kanishka <kanishkab2917@gmail.com>
2024-07-11 17:06:50 +0530

    string buffer using append completed with no errors

--- 16_StringBufferClas/src/com/bsl/stringbuffer/StringBufferDemo.java
+++ 16_StringBufferClas/src/com/bsl/stringbuffer/StringBufferDemo.java
@@ -3,6 +3,22 @@ package com.bsl.stringbuffer;
 public class StringBufferDemo {
 
 	public static void main(String[] args) {
+
+		StringBuffer sb1 = new StringBuffer("hello");
+		System.out.println(sb1);
+		System.out.println(sb1.length());
+		System.out.println(sb1.capacity());
+		sb1.append("1234");
+		System.out.println(sb1);
+		sb1.replace(2, 4, "cluster");
+		System.out.println("after replacing"+sb1);
+		sb1.delete(3, 6);
+		System.out.println(sb1);
+		sb1.insert(5, "a");
+		System.out.println("after inserting"+sb1);
+		sb1.reverse();
+		System.out.println(sb1);
+
 	}
 
 }
