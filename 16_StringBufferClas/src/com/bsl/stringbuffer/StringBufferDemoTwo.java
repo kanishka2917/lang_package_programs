42098ea91d7513e484bba9992fd4b0b57703936d
Kanishka <kanishkab2917@gmail.com>
2024-07-11 17:33:10 +0530

    string buffer equals DemoTwo completed

--- 16_StringBufferClas/src/com/bsl/stringbuffer/StringBufferDemoTwo.java
+++ 16_StringBufferClas/src/com/bsl/stringbuffer/StringBufferDemoTwo.java
@@ -3,8 +3,16 @@ package com.bsl.stringbuffer;
 public class StringBufferDemoTwo {
 
 	public static void main(String[] args) {
-		
-		
+
+		StringBuffer sb2 = new StringBuffer("hello");
+		StringBuffer sb3 = new StringBuffer("hello");
+		System.out.println("sb2 & sb3 are equal " + (sb2 == sb3));
+		System.out.println("sb2 & sb3 are equal " + (sb2.equals(sb3)));
+		String s1 = sb2.toString();
+		String s2 = sb3.toString();
+		System.out.println("s1 and s2 are equal " + s1.equals(s2));
+		System.out.println((s1 == s2));
+
 	}
 
 }
