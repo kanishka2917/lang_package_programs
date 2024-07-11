51e922bcd8ca457af501dc9474f2c5fc289fc1fd
Kanishka <kanishkab2917@gmail.com>
2024-07-11 17:25:22 +0530

    string buffer DemoThree completed

--- 16_StringBufferClas/src/com/bsl/stringbuffer/DemoThree.java
+++ 16_StringBufferClas/src/com/bsl/stringbuffer/DemoThree.java
@@ -3,7 +3,17 @@ package com.bsl.stringbuffer;
 public class DemoThree {
 
 	public static void main(String[] args) {
-		
+
+		StringBuffer sb1 = new StringBuffer("hello");
+		StringBuffer sb2 = new StringBuffer("hello");
+		if (sb1 == sb2) {
+			System.out.println("inside upper if block");
+
+		}
+		if (sb1.equals(sb2)) {
+			System.out.println("inside lower if block");
+
+		}
 	}
 
 }
