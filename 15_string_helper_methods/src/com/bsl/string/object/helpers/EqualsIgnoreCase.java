12ce466be097e3fdf9b0c5dfab0cd16b07e52ff2
Kanishka <kanishkab2917@gmail.com>
2024-07-11 09:50:15 +0530

    equals ignore case is so simple

--- 15_string_helper_methods/src/com/bsl/string/object/helpers/EqualsIgnoreCase.java
+++ 15_string_helper_methods/src/com/bsl/string/object/helpers/EqualsIgnoreCase.java
@@ -2,9 +2,12 @@ package com.bsl.string.object.helpers;
 
 public class EqualsIgnoreCase {
 
-	public static void main(String[] args) 
-	{
-		
+	public static void main(String[] args) {
+
+		String s11 = new String("hello");
+		String s12 = new String("Hello");
+		System.out.println("checking ignoring case : " + s11.equalsIgnoreCase(s12));
+
 	}
 
 }
