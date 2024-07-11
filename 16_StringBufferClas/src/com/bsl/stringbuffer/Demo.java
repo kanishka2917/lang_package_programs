e381ae3c920051b59b0b4c8835eae5a40f95fa20
Kanishka <kanishkab2917@gmail.com>
2024-07-11 16:26:28 +0530

    trying out string buffer class

---
+++ 16_StringBufferClas/src/com/bsl/stringbuffer/Demo.java
@@ -0,0 +1,14 @@
+package com.bsl.stringbuffer;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		StringBuffer sb1 = new StringBuffer();
+		System.out.println(sb1);
+		System.out.println(sb1.length());
+		System.out.println(sb1.capacity());
+		
+		
+	}
+
+}
