4e39d12a1fe7fdaf4b20bac7e08a7f083f2a38ef
Kanishka <kanishkab2917@gmail.com>
2024-07-11 16:58:51 +0530

    example two of string buffer class completed

--- 16_StringBufferClas/src/com/bsl/stringbuffer/DemoTwo.java
+++ 16_StringBufferClas/src/com/bsl/stringbuffer/DemoTwo.java
@@ -3,7 +3,15 @@ package com.bsl.stringbuffer;
 public class DemoTwo {
 
 	public static void main(String[] args) {
-		
-				}
+
+		// method two
+		StringBuffer sb2 = new StringBuffer(30);// any value
+		System.out.println(sb2);
+		System.out.println(sb2.length());
+		System.out.println(sb2.capacity());
+		sb2.append("hello");
+		System.out.println(sb2);
+
+	}
 
 }
