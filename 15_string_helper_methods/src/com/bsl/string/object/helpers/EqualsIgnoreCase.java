e9e7e3004fe47f84af21ca1129a8cbaee128366b
Kanishka <kanishkab2917@gmail.com>
2024-07-11 09:48:50 +0530

    start of toupper and tolower

---
+++ 15_string_helper_methods/src/com/bsl/string/object/helpers/EqualsIgnoreCase.java
@@ -0,0 +1,12 @@
+package com.bsl.string.object.helpers;
+
+public class EqualsIgnoreCase {
+
+	public static void main(String[] args) {
+		
+		String s11 = new String ("hello");
+		String s12 = new String ("Hello");
+		System.out.println("checking ignoring case : " + s11.equalsIgnoreCase(s12));
+	}
+
+}
