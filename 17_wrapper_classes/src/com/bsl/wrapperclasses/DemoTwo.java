e10c96dd59835651e976344f33f8e3fca2cf2c9a
Kanishka <kanishkab2917@gmail.com>
2024-07-13 16:53:35 +0530

    succesfully checked equality of primitive vs user-defined using if-else-if

--- 17_wrapper_classes/src/com/bsl/wrapperclasses/DemoTwo.java
+++ 17_wrapper_classes/src/com/bsl/wrapperclasses/DemoTwo.java
@@ -2,6 +2,20 @@ package com.bsl.wrapperclasses;
 
 public class DemoTwo {
 	public static void main(String[] args) {
-
+		int i = 10;
+		Integer x = new Integer(i);
+		System.out.println(i);
+		System.out.println(x);
+		int j = 20;
+		Integer y = new Integer(j);
+		System.out.println(j);
+		System.out.println(y);
+		if (i > j) {
+			System.out.println("i greater than j");
+		} else if (i < j) {
+			System.out.println("i less than j");
+		} else {
+			System.out.println("both are equal");
+		}
 	}
 }
