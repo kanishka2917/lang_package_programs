f817bd1d9918eb299f17a07c8d660ce427f48e59
Kanishka <kanishkab2917@gmail.com>
2024-07-13 16:44:25 +0530

    checked equality of primitive vs user-defined using ==

--- 17_wrapper_classes/src/com/bsl/wrapperclasses/DemoOne.java
+++ 17_wrapper_classes/src/com/bsl/wrapperclasses/DemoOne.java
@@ -4,7 +4,20 @@ public class DemoOne {
 
 	public static void main(String[] args) {
 		
-		
+		int i = 10;
+		Integer x = new Integer(i);
+		System.out.println(i);
+		System.out.println(x);
+		int j = 20;
+		Integer y = new Integer(j);
+		System.out.println(j);
+		System.out.println(y);
+		if (i == x) {
+			System.out.println("equal");	
+		}
+		else {
+			System.out.println("not equal");
+		}
 	}
 
 }
