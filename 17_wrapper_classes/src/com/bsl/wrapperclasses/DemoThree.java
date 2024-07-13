6ec2d233bcf96f88dbfb67f6994584c70feaad06
Kanishka <kanishkab2917@gmail.com>
2024-07-13 17:02:50 +0530

    intValue() method is working

--- 17_wrapper_classes/src/com/bsl/wrapperclasses/DemoThree.java
+++ 17_wrapper_classes/src/com/bsl/wrapperclasses/DemoThree.java
@@ -2,10 +2,15 @@ package com.bsl.wrapperclasses;
 
 public class DemoThree {
 
-	public static void main(String[] args) 
-	{
-		
-		
+	public static void main(String[] args) {
+
+		Integer x = new Integer(10);
+		Integer y = new Integer(20);
+		System.out.println(x.equals(y));
+		int p = x.intValue();
+		int q = y.intValue();
+		System.out.println(p + q);
+
 	}
 
 }
