5a9a1a06b71764f129e2a0d7a9dbba2120a47559
Kanishka <kanishkab2917@gmail.com>
2024-07-30 23:19:59 +0530

    learnt to convert primitive to wrapper objects

--- 18_Core java/src/com/bsl/wrapper/classes/demos/WrapperDemoOne.java
+++ 18_Core java/src/com/bsl/wrapper/classes/demos/WrapperDemoOne.java
@@ -3,8 +3,23 @@ package com.bsl.wrapper.classes.demos;
 public class WrapperDemoOne {
 
 	public static void main(String[] args) {
-		
-		
+
+		int i = 10;
+		Integer x = new Integer(i);
+		Integer p = Integer.valueOf(i);
+		System.out.println(i);
+		System.out.println(x);
+		System.out.println(p);
+		double d = 99.99;
+		double y = new Double(d);
+		double q = Double.valueOf(d);
+		System.out.println(d);
+		System.out.println(y);
+		System.out.println(q);
+		boolean b = true;
+		boolean z = new Boolean(b);
+		boolean r = Boolean.valueOf(b);
+
 	}
 
 }
