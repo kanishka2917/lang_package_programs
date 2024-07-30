da7840ba68d9e52b1aeab6cedef415940b2fbe85
Kanishka <kanishkab2917@gmail.com>
2024-07-30 23:26:04 +0530

    using value() method we can convert

--- 18_Core java/src/com/bsl/wrapper/classes/demos/WrapperDemoTwo.java
+++ 18_Core java/src/com/bsl/wrapper/classes/demos/WrapperDemoTwo.java
@@ -4,8 +4,18 @@ public class WrapperDemoTwo {
 
 	public static void main(String[] args) {
 		
-		
-		
+		int i = 999;
+		Integer z = new Integer(i);
+		byte b = z.byteValue();
+		short s = z.shortValue();
+		long l = z.longValue();
+		float f = z.floatValue();
+		double d = z.doubleValue();
+		System.out.println(b);
+		System.out.println(l);
+		System.out.println(d);
+		System.out.println(s);
+		System.out.println(f);
 	}
 
 }
