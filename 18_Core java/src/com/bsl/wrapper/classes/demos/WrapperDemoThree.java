f129eaf0cef852004c82ae9ce3fd5319bf9749f6
Kanishka <kanishkab2917@gmail.com>
2024-07-30 23:34:12 +0530

    figured out how to convert primitive to string

--- 18_Core java/src/com/bsl/wrapper/classes/demos/WrapperDemoThree.java
+++ 18_Core java/src/com/bsl/wrapper/classes/demos/WrapperDemoThree.java
@@ -4,6 +4,16 @@ public class WrapperDemoThree {
 
 	public static void main(String[] args) {
 		
+		int x = 99;
+		String s1 = Integer.toString(x);
+		byte b = 10;
+		Byte bb = new Byte(b);
+		String s2 = bb.toString();
+		Short s = 12;
+		String s4 = String.valueOf(s);
+		double d = 99.99;
+		Double dd = new Double (d);
+		
 		
 	}
 
