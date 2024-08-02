d5eedd0dc75eea2e426e9a62ec87c47ddbb97b9a
Kanishka <kanishkab2917@gmail.com>
2024-08-02 22:26:20 +0530

    Renaming my files to  look formatted

---
+++ 20_exceptional handling 2/src/com/cluster/bsl/trows/ExceptionalHandlingTweleve..java
@@ -0,0 +1,16 @@
+package com.bsl.string.object.helpers;
+
+public class ReplaceDemo {
+
+	public static void main(String[] args) {
+		
+		String s1 ="apple";
+		System.out.println(s1);
+		String subs1 = s1.substring(2,4);
+		System.out.println(s1);
+		System.out.println(subs1);
+		System.out.println(s1.substring(2));
+		
+	}
+
+}
