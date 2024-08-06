7691ef6073aae386a76a781832a7b0435e1b8194
Kanishka <kanishkab2917@gmail.com>
2024-08-06 23:25:23 +0530

    Renaming my files to  look formatted

---
+++ 19_exceptional handling - Copy/src/com/cluster/bsl/trows/ExceptionalHandlingFifteen..java
@@ -0,0 +1,14 @@
+package com.bsl.string.object.helpers;
+
+public class TrimDemo {
+
+	public static void main(String[] args) {
+		
+		String s2 = "        ab c";
+		System.out.println(s2);
+		String s3 = s2.trim();
+		System.out.println(s2);
+		System.out.println(s3);
+	}
+
+}
