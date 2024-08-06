145ba31d02d0d0823894e3ae39d3c7c78580277e
Kanishka <kanishkab2917@gmail.com>
2024-08-06 23:34:07 +0530

    Renaming my files to  look formatted

---
+++ 27_exceptional handling 9 - Copy/src/com/cluster/bsl/trows/ExceptionalHandlingEight..java
@@ -0,0 +1,14 @@
+package com.bsl.string.object.helpers;
+
+public class UpperAndLower {
+
+	public static void main(String[] args) {
+	
+		String s10 = "HelloWorld";
+		s10 = s10.toLowerCase();
+		System.out.println(s10);
+		s10 = s10.toUpperCase();
+		System.out.println(s10);
+	}
+
+}
