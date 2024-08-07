cedfd6d10f1de64cbc07f88da20dd7098412ed31
Kanishka <kanishkab2917@gmail.com>
2024-08-07 23:32:57 +0530

    Renaming my files to  look formatted

---
+++ 19_exceptional handling - Copy/src/com/cluster/bsl/trows/ExceptionalHandlingEleven..java
@@ -0,0 +1,18 @@
+package com.bsl.string.object.helpers;
+
+import javax.management.StringValueExp;
+
+public class ValueOfDemo {
+
+	public static void main(String[] args) {
+		
+		String s8 = String.valueOf(99);
+		boolean b = true;
+		String s9 = String.valueOf(b);
+		System.out.println(s8);
+		System.out.println(s9);
+
+
+	}
+
+}
