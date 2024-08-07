f5812733cd5c651f3ebbefa04c510d9a6c00a223
Kanishka <kanishkab2917@gmail.com>
2024-08-07 23:27:38 +0530

    Renaming my files to  look formatted

---
+++ 19_exceptional handling - Copy/src/com/cluster/bsl/trows/ExceptionalHandlingSixteen..java
@@ -0,0 +1,13 @@
+package com.bsl.string.object.helpers;
+
+public class StartWithAndEndWith {
+
+	public static void main(String[] args) {
+		
+		String s5 = "cluster";
+		System.out.println(s5.startsWith("clu"));
+		System.out.println(s5.endsWith("ter"));
+
+	}
+
+}
