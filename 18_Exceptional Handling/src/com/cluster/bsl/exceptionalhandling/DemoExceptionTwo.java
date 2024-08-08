8d5ee36a0c9208c0d723af54d333721861693f25
Kanishka <kanishkab2917@gmail.com>
2024-08-08 23:08:06 +0530

    getting an ArrayIndexOutOfBoundException

--- 18_Exceptional Handling/src/com/cluster/bsl/exceptionalhandling/DemoExceptionTwo.java
+++ 18_Exceptional Handling/src/com/cluster/bsl/exceptionalhandling/DemoExceptionTwo.java
@@ -3,18 +3,26 @@ package com.cluster.bsl.exceptionalhandling;
 public class DemoExceptionTwo {
 
 	public static void main(String[] args) {
-		
+
 		int i, j, k = 0;
 		i = 8;
-		j = 0;
+		j = 2;
 		try {
 			k = i / j;
+			for (int c = 0; c <= 4; c++) {
+				a[c] = c + 1;
+			}
+			for (int value : a) {
+				System.out.println(value);
+			}
+		} catch (Exception e) {
+			System.out.println("cannot divide by zero"+e);
 		}
-		catch(Exception e) {
-			System.out.println("cannot divide by zero");
+		catch(ArrayIndexOutOfBoundException e) {
+			System.out.println("maximum number of values is 4");
 		}
 		System.out.println(k);
-	
+
 	}
 
 }
