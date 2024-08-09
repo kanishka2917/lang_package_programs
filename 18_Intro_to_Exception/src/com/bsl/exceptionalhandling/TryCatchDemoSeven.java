681d4740c5ef0a05815e663c419edb3ad5261ebb
Kanishka <kanishkab2917@gmail.com>
2024-08-09 18:37:33 +0530

    practising another try catch demo

--- 18_Intro_to_Exception/src/com/bsl/exceptionalhandling/TryCatchDemoSeven.java
+++ 18_Intro_to_Exception/src/com/bsl/exceptionalhandling/TryCatchDemoSeven.java
@@ -3,19 +3,16 @@ package com.bsl.exceptionalhandling;
 public class TryCatchDemoSeven {
 
 	public static void main(String[] args) {
-		
+
 		int a = 10;
 		int b = 0;
 		ExceptionalDemoSix h = new ExceptionalDemoSix();
 		try {
-			h.m1(a,b);
-		}
-		catch(ArithmeticException e) {
+			h.m1(a, b);
+		} catch (ArithmeticException e) {
 			int p = 10;
 			int q = 2;
 			h.m1(p, q);
-			
 		}
 	}
-
 }
