cf6023fc70e1b8b6e5d51ffba6b6c8f6120be23e
Kanishka <kanishkab2917@gmail.com>
2024-08-09 17:39:38 +0530

    the catch gets executed only executes if any error occurs

--- 18_Intro_to_Exception/src/com/bsl/exceptionalhandling/ExceptionDemoTwo.java
+++ 18_Intro_to_Exception/src/com/bsl/exceptionalhandling/ExceptionDemoTwo.java
@@ -4,7 +4,17 @@ public class ExceptionDemoTwo {
 
 	public static void main(String[] args) {
 		
-		
+		try {
+			int x = 10;
+			int y = 0;
+			int z = 0;
+			z = x / y;
+		}
+		catch(ArithmeticException e) {
+			System.out.println(e);
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
+		System.out.println("After catch block");
 	}
-
 }
