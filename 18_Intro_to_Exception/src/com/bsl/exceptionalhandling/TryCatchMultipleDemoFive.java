adba6c84b8f3ab995c1b7671e8184914f28e19e3
Kanishka <kanishkab2917@gmail.com>
2024-08-09 18:12:48 +0530

    in the first step of solving the eceptions

--- 18_Intro_to_Exception/src/com/bsl/exceptionalhandling/TryCatchMultipleDemoFive.java
+++ 18_Intro_to_Exception/src/com/bsl/exceptionalhandling/TryCatchMultipleDemoFive.java
@@ -4,7 +4,24 @@ public class TryCatchMultipleDemoFive {
 
 	public static void main(String[] args) {
 		
-		
+		try {
+			int x = 10;
+			int y = 2;
+			int z = 0;
+			z = x / y;
+			System.out.println("result of div is :" + z);
+		}
+		catch(ArithmeticException e) {
+			System.out.println(e);
+		}
+		catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println(e);
+		}
+		catch(Exception e) {
+			System.out.println("inside catch of exception");
+			e.printStackTrace();
+		}
+		System.out.println("end main");
 	}
 
 }
