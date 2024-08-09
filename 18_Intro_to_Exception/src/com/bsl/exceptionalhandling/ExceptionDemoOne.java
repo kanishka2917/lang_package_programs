a10451095de8342ca37d061507db316f5c35a8d5
Kanishka <kanishkab2917@gmail.com>
2024-08-09 17:30:23 +0530

    Arithemetic exception to be solved in the following programs

--- 18_Intro_to_Exception/src/com/bsl/exceptionalhandling/ExceptionDemoOne.java
+++ 18_Intro_to_Exception/src/com/bsl/exceptionalhandling/ExceptionDemoOne.java
@@ -4,6 +4,14 @@ public class ExceptionDemoOne {
 	
 	public static void main(String[] args) {
 		
+		int x = 10;
+		int y = 0;
+		int z = 0;
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		z = x / y;//Arithemetic exception
+		System.out.println("end of main");
 		
 	}
 
