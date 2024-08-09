e080c722fb924a137055f837f35d71e846365646
Kanishka <kanishkab2917@gmail.com>
2024-08-09 18:35:34 +0530

    going forward with another try catch demo

--- 18_Intro_to_Exception/src/com/bsl/exceptionalhandling/TryCatchDemoSeven.java
+++ 18_Intro_to_Exception/src/com/bsl/exceptionalhandling/TryCatchDemoSeven.java
@@ -4,6 +4,18 @@ public class TryCatchDemoSeven {
 
 	public static void main(String[] args) {
 		
+		int a = 10;
+		int b = 0;
+		ExceptionalDemoSix h = new ExceptionalDemoSix();
+		try {
+			h.m1(a,b);
+		}
+		catch(ArithmeticException e) {
+			int p = 10;
+			int q = 2;
+			h.m1(p, q);
+			
+		}
 	}
 
 }
