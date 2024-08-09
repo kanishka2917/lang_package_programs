83376bee821ba31105ef4222bcb4400675a568b8
Kanishka <kanishkab2917@gmail.com>
2024-08-09 18:25:11 +0530

    learnt to solve eceptions in caller method

--- 18_Intro_to_Exception/src/com/bsl/exceptionalhandling/ExceptionalDemoSix.java
+++ 18_Intro_to_Exception/src/com/bsl/exceptionalhandling/ExceptionalDemoSix.java
@@ -2,9 +2,22 @@
 package com.bsl.exceptionalhandling;
 
 public class ExceptionalDemoSix {
-	
-	public static void main(String[] args) {
-		
-		
+			
+		void m1(int x, int y)
+		{
+			try
+			{
+				int z = 0;
+				z = x / y;
+				System.out.println("result of div is " + x);
+			}
+			catch(ArithmeticException e)
+			{
+				System.out.println("please give correct value");
+				int p = 10;
+				int q = 2;
+				int r = p / 2;
+				System.out.println("end of m1");
+			}
 	}
 }
