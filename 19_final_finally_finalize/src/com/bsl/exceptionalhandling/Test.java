207b1724f321f7f7b562275477a01c9e007b5926
Kanishka <kanishkab2917@gmail.com>
2024-08-10 20:51:15 +0530

    winding up the object class with some logic for the following program

--- 19_final_finally_finalize/src/com/bsl/exceptionalhandling/Test.java
+++ 19_final_finally_finalize/src/com/bsl/exceptionalhandling/Test.java
@@ -1,5 +1,17 @@
 package com.bsl.exceptionalhandling;
 
 public class Test {
+	
+	void m1(int x, int y) {
+		System.out.println("begin m1");
+		try {
+			System.out.println("inside try");
+			int z = x / y;
+			System.out.println("result of div is " + z);
+		}
+		catch(ArithmeticException e){
+			System.out.println("inside catch" + e);
+		}
+	}
 
 }
