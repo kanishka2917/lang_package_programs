4a0f03bee407f0d41ed2a21def2facbfbd17371f
Kanishka <kanishkab2917@gmail.com>
2024-08-10 23:31:28 +0530

    finally executes with no exception

--- 19_final_finally_finalize/src/com/bsl/exceptionalhandling/FinallyDemoThree.java
+++ 19_final_finally_finalize/src/com/bsl/exceptionalhandling/FinallyDemoThree.java
@@ -3,8 +3,9 @@ package com.bsl.exceptionalhandling;
 public class FinallyDemoThree {
 
 	public static void main(String[] args) {
-		
-	
-	}
 
+		Fun f = new Fun();
+		int res = f.m1(10, 0);
+		System.out.println("val of res is " + res);
+	}
 }
