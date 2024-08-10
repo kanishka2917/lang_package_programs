ad6fae7bb2b0431776652414f76a098732c70a63
Kanishka <kanishkab2917@gmail.com>
2024-08-10 23:27:39 +0530

    class Fun object completed

--- 19_final_finally_finalize/src/com/bsl/exceptionalhandling/Fun.java
+++ 19_final_finally_finalize/src/com/bsl/exceptionalhandling/Fun.java
@@ -1,5 +1,23 @@
 package com.bsl.exceptionalhandling;
 
 public class Fun {
-
+	
+	int m1(int x, int y) {
+		int z = 0;
+		try {
+			System.out.println("begin try");
+			z = x / y;
+			return z;
+		}
+		catch(ArithmeticException e) {
+			System.out.println("inside catch");
+			return z;
+		}
+		finally {
+			System.out.println("inside finally");
+			System.out.println(x);
+			System.out.println(y);
+			System.out.println(z);
+		}
+	}
 }
