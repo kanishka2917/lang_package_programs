5a71d6848e400585aa33127699d349a50d99820c
Kanishka <kanishkab2917@gmail.com>
2024-08-10 23:42:24 +0530

    class z is ready for execution with no errors

--- 19_final_finally_finalize/src/com/bsl/exceptionalhandling/Z.java
+++ 19_final_finally_finalize/src/com/bsl/exceptionalhandling/Z.java
@@ -2,4 +2,15 @@ package com.bsl.exceptionalhandling;
 
 public class Z {
 
+	int m1(int x, int y) {
+		int z = 0;
+		try {
+			System.out.println("begin try");
+			z = x/y;
+			return z;
+		}
+		finally {
+			System.out.println("inside finally");
+		}
+	}
 }
