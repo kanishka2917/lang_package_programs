62fb77551929da73dbfb5b842f270242a07937c8
Kanishka <kanishkab2917@gmail.com>
2024-08-10 23:51:42 +0530

    figured that Exception skips the try method in the program

--- 19_final_finally_finalize/src/com/bsl/exceptionalhandling/FinallyFixErrorDDemoFour.java
+++ 19_final_finally_finalize/src/com/bsl/exceptionalhandling/FinallyFixErrorDDemoFour.java
@@ -3,6 +3,22 @@ package com.bsl.exceptionalhandling;
 public class FinallyFixErrorDDemoFour {
 
 	public static void main(String[] args) {
+
+		System.out.println("begin main");
+		Z z = null;
+		int res = 0;
+		try {
+			System.out.println("begin try");
+			z = new Z();
+			res = z.m1(10, 0);
+			System.out.println("result of division" + res);
+			System.out.println("end try");
+		} catch (ArithmeticException e) {
+			System.out.println("inside catch");
+			res = z.m1(10, 2);
 		}
+		System.out.println("end main");
+
+	}
 
 }
