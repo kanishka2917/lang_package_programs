9c1fe781129fa71f676bcfa3b5572ce941509055
Kanishka <kanishkab2917@gmail.com>
2024-08-12 22:52:19 +0530

    AIOBE as expected, proceding to solve them in further programs

--- 20_throw_throws/src/com/bsl/throwandthow/ThrowsDemoOne.java
+++ 20_throw_throws/src/com/bsl/throwandthow/ThrowsDemoOne.java
@@ -3,8 +3,17 @@ package com.bsl.throwandthow;
 public class ThrowsDemoOne {
 
 	public static void main(String[] args) {
-		
-		
+
+		System.out.println("begin main");
+		try {
+			A a = new A();
+			a.m1(10, 2);
+		} catch (ArithmeticException e) {
+			System.out.println(e);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println(e);
+		}
+		System.out.println("end");
 	}
 
 }
