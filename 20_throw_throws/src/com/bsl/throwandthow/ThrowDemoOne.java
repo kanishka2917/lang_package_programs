8a5093ef12abde255434d53071f70c395fc33d4d
Kanishka <kanishkab2917@gmail.com>
2024-08-12 23:06:04 +0530

    error free program, with clear excecution

--- 20_throw_throws/src/com/bsl/throwandthow/ThrowDemoOne.java
+++ 20_throw_throws/src/com/bsl/throwandthow/ThrowDemoOne.java
@@ -4,7 +4,14 @@ public class ThrowDemoOne {
 
 	public static void main(String[] args) {
 		
-		
+		Hai h = new Hai();
+		try {
+			h.m1(10, 0);
+			System.out.println("end try");
+		}
+		catch(ArithmeticException e) {
+			h.m1(10, 2);
+		}
 	}
 
 }
