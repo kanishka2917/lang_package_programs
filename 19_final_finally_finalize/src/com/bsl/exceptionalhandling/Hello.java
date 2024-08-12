e5e990b22a2cd1e4d0368744af485d1b2bb5753d
Kanishka <kanishkab2917@gmail.com>
2024-08-12 22:01:35 +0530

    deleting unwanted files from the branch

--- 19_final_finally_finalize/src/com/bsl/exceptionalhandling/Hello.java
+++ 19_final_finally_finalize/src/com/bsl/exceptionalhandling/Hello.java
@@ -1,5 +1,14 @@
 package com.bsl.exceptionalhandling;
 
-public class Hello {
+import java.sql.Connection;
 
+public class Hello {
+	
+	Connection con;
+	Hello(){	
+		con = get db Connection;
+	}
+	m1(){
+		us
+	}
 }
