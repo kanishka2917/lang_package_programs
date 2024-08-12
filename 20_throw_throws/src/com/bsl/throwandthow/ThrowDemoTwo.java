bc9f75279237e508a25e93d83532a117e0ee41ff
Kanishka <kanishkab2917@gmail.com>
2024-08-12 23:12:26 +0530

    program executed with no errors and exception

--- 20_throw_throws/src/com/bsl/throwandthow/ThrowDemoTwo.java
+++ 20_throw_throws/src/com/bsl/throwandthow/ThrowDemoTwo.java
@@ -3,6 +3,19 @@ package com.bsl.throwandthow;
 public class ThrowDemoTwo {
 
 	public static void main(String[] args) {
+
+		System.out.println("begin main");
+		String s1 = "hello";
+		int x = 999;
+		try {
+			int p = Integer.parseInt(s1);
+			System.out.println(p);
 		}
+		catch(NumberFormatException e) {
+			String s2 = "919";
+			int pt = Integer.parseInt(s2);
+			System.out.println(x);
+		}
+	}
 
 }
