e61ce369b37c9b30cd68c6a7ddbb179544c392b4
Kanishka <kanishkab2917@gmail.com>
2024-08-13 23:40:22 +0530

    trying to solve my errors with more knowledge about the code

--- 21_intro_to_io.package/src/com/bsl/iopackages/A.java
+++ 21_intro_to_io.package/src/com/bsl/iopackages/A.java
@@ -2,4 +2,18 @@ package com.bsl.iopackages;
 
 public class A {
 
+	public int m1(int x, int y) {
+		try {
+			if(x>y) {
+				System.out.println("x is greater than y");
+			}
+			else {
+			}
+		}
+	}
+}
+class B extends A{
+	public int m1(int x, int y) {
+	
+	}
 }
