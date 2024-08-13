93af8fa92c586393adde6d2031aa830285d6e942
Kanishka <kanishkab2917@gmail.com>
2024-08-13 23:15:49 +0530

    gained some knowledge about system.exit

---
+++ 21_intro_to_io.package/src/com/bsl/iopackages/DemoOne.java
@@ -0,0 +1,13 @@
+package com.bsl.iopackages;
+
+public class DemoOne {
+
+	public static void main(String[] args) {
+
+		System.out.println("begin main");
+		System.exit(0);
+		System.out.println("end main");
+
+	}
+
+}
