c21085daee512620cddf1912d2d60c6593c8edb0
Kanishka <kanishkab2917@gmail.com>
2024-08-13 23:20:03 +0530

    output type no.1

--- 21_intro_to_io.package/src/com/bsl/iopackages/DemoTwo.java
+++ 21_intro_to_io.package/src/com/bsl/iopackages/DemoTwo.java
@@ -3,6 +3,13 @@ package com.bsl.iopackages;
 public class DemoTwo {
 
 	public static void main(String[] args) {
+	
+		try {
+			System.out.println("begin main");
+			return;
+		}
+		finally {
+			System.out.println("end main");
 		}
 	}
 
