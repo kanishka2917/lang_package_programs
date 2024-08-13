cc2a273a8e635beff50ec4f9f145977682f81aa6
Kanishka <kanishkab2917@gmail.com>
2024-08-14 00:00:14 +0530

    trying to solve these errors

--- 21_intro_to_io.package/src/com/bsl/iopackages/BufferedReaderDemo.java
+++ 21_intro_to_io.package/src/com/bsl/iopackages/BufferedReaderDemo.java
@@ -1,10 +1,20 @@
 package com.bsl.iopackages;
 
+import java.io.InputStream;
+
 public class BufferedReaderDemo {
 
 	public static void main(String[] args) {
 		
-	
+		try {
+			System.out.println("please enter a value");
+			String s1 = reader.Readline();
+			int x = Integer.parseInt(s1);
+			System.out.println("please enter second value");
+			String s2 = reader.Readline();
+			
+			
+		}
 	}
 
 }
