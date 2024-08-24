4d789b50a71de5271116b50bd3b951106dc134b7
Kanishka <kanishkab2917@gmail.com>
2024-08-24 18:02:01 +0530

    Absolutely not, both the exceptions are perectly handled

--- 26_multiple_try_catch/src/com/bsl/multiple/Handling.java
+++ 26_multiple_try_catch/src/com/bsl/multiple/Handling.java
@@ -4,7 +4,19 @@ public class Handling {
 
 	public static void main(String[] args) {
 		// Multiple try catch
-		
+		try {
+			int a = 10, b = 0, c;
+			c = a / b;
+			System.out.println(c);
+		} catch (ArithmeticException a) {
+			System.out.println("cant divide by zero");
+		}
+		try {
+			int a[] = { 10, 20, 30, 40 };
+			System.out.println(a[5]);
+		} catch (ArrayIndexOutOfBoundsException b) {
+			System.out.println("beyond the array limit");
+		}
 
 	}
 
