ccb1617a7c1cdb9425a764c392983fb398bf9c6f
Kanishka <kanishkab2917@gmail.com>
2024-08-24 16:58:37 +0530

    arithmetic exception is handled using throw and catch method

--- 22_Exceptional_Handling/src/com/bsl/exceptionalhandling/B.java
+++ 22_Exceptional_Handling/src/com/bsl/exceptionalhandling/B.java
@@ -1,9 +1,21 @@
 package com.bsl.exceptionalhandling;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class B {
 
 	public static void main(String[] args) {
 		
+		System.out.println("main method started");
+		int a = 10, b = 0,c;
+		try {
+			c = a / b;
+			System.out.println(c);
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
+		System.out.println("main method ended");
 	}
 
 }
