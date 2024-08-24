09277eb8633e30364ebeb315ba8733514541f1fa
Kanishka <kanishkab2917@gmail.com>
2024-08-24 17:50:16 +0530

    analyzing what try catch finally methods actually do

--- 25_try_catch_finally/src/com/bsl/Handling.java
+++ 25_try_catch_finally/src/com/bsl/Handling.java
@@ -3,7 +3,21 @@ package com.bsl;
 public class Handling {
 
 	public static void main(String[] args) {
-		
+		try
+		{
+			System.out.println("learning coding");
+			int a = 20, b = 0, c;
+			c = a / b;
+			System.out.println(c);
+			System.out.println("hello");
+		}
+		catch(ArithmeticException a) {
+			System.out.println("cannot divide by zero");
+		}
+		finally {
+			System.out.println("im finally");
+		}
+		System.out.println("main method ended");
 	}
 
 }
