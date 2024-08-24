420f8c3c693b8fcbf4586bb175246a906ee44a99
Kanishka <kanishkab2917@gmail.com>
2024-08-24 17:06:06 +0530

    got an NullPointerException which im trying to solve

---
+++ 23_NullPointerException/src/com/bsl/nullpointerexception/NullPointerException.java
@@ -0,0 +1,11 @@
+package com.bsl.nullpointerexception;
+
+public class NullPointerException {
+
+	public static void main(String[] args) {
+		
+		String str =  null;
+		System.out.println(str.toUpperCase());
+	}
+
+}
