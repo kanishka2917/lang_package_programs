e7511ed93fa67dc470dd42aa68c6d615c7cb8f4b
Kanishka <kanishkab2917@gmail.com>
2024-08-24 18:50:05 +0530

    getting an arithmetic exception

--- 28_nested_try_block/src/com/bsl/nestedtryblock/NestedTry.java
+++ 28_nested_try_block/src/com/bsl/nestedtryblock/NestedTry.java
@@ -3,8 +3,20 @@ package com.bsl.nestedtryblock;
 public class NestedTry {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try {
+			try {
+				int a[]= {10,20,30};
+				System.out.println(a[2]);
+			}
+			catch (ArrayIndexOutOfBoundsException a) {
+				System.out.println(a);
+			}
+			System.err.println(10/0);
+		}
+		catch (ArithmeticException e) {
+			System.out.println(e);
+		}
+		System.out.println("end of main");
 	}
 
 }
