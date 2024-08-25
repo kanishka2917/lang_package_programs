78652c4fc7a12582b36958f918652146cdcb2be3
Kanishka <kanishkab2917@gmail.com>
2024-08-25 13:00:17 +0530

    getting an Arithemetic exception

--- 39_Exception_propogation/src/com/bsl/Demo.java
+++ 39_Exception_propogation/src/com/bsl/Demo.java
@@ -3,7 +3,14 @@ package com.bsl;
 public class Demo {
 
 	public static void main(String[] args) {
-		
+		m1();
 	}
 
+	public static void m1() {
+		m2();
+	}
+
+	public static void m2() {
+		System.out.println(10 / 0);
+	}
 }
