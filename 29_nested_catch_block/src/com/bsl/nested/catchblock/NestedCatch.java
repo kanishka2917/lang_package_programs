7abf5c5e62e31d9e46af47a5780a1f9004b4e71a
Kanishka <kanishkab2917@gmail.com>
2024-08-25 11:31:10 +0530

    handeled all the possible exceptions

--- 29_nested_catch_block/src/com/bsl/nested/catchblock/NestedCatch.java
+++ 29_nested_catch_block/src/com/bsl/nested/catchblock/NestedCatch.java
@@ -5,10 +5,11 @@ public class NestedCatch {
 	public static void main(String[] args) {
 
 		try {
-			System.out.println(10 / 0);
+			System.out.println(10 / 2);
 		} catch (Exception e) {
+			System.out.println(e);
 			try {
-				String a = null;
+				String a = "ANU";
 				System.out.println(a.toLowerCase());
 			} catch (NullPointerException n) {
 				System.err.println("null value cant be converted");
