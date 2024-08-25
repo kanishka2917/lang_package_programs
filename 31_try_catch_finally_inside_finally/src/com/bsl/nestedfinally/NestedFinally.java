ddb56108dbf03e6a40e87c5b4d725d0a09a91942
Kanishka <kanishkab2917@gmail.com>
2024-08-25 11:39:00 +0530

    expected ArithemeticException trynna solve it

--- 31_try_catch_finally_inside_finally/src/com/bsl/nestedfinally/NestedFinally.java
+++ 31_try_catch_finally_inside_finally/src/com/bsl/nestedfinally/NestedFinally.java
@@ -3,8 +3,16 @@ package com.bsl.nestedfinally;
 public class NestedFinally {
 
 	public static void main(String[] args) {
-		
-		
+
+		try {
+			String a = "anu";
+			System.out.println(a.toUpperCase());
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			System.out.println(10 / 0);
+		}
+		System.out.println("main method ended");
 	}
 
 }
