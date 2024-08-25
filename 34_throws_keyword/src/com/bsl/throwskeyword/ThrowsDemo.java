e50725a8dae605f3862f0d2b0e917f148c953574
Kanishka <kanishkab2917@gmail.com>
2024-08-25 12:05:24 +0530

    solving the same exception using try and catch and getting my desired output

--- 34_throws_keyword/src/com/bsl/throwskeyword/ThrowsDemo.java
+++ 34_throws_keyword/src/com/bsl/throwskeyword/ThrowsDemo.java
@@ -2,11 +2,15 @@ package com.bsl.throwskeyword;
 
 public class ThrowsDemo {
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) {
 
 		for (int i = 1; i <= 10; i++) {
-			System.out.println(i);
-			Thread.sleep(1000);
+			try {
+				System.out.println(i);
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				System.out.println(i);
+			}
 		}
 	}
 }
