e11cbc68f28ecc6a75e764f784c50bde39feb40b
Kanishka <kanishkab2917@gmail.com>
2024-08-25 11:25:31 +0530

    NPE occured trying to solve

--- 29_nested_catch_block/src/com/bsl/nested/catchblock/NestedCatch.java
+++ 29_nested_catch_block/src/com/bsl/nested/catchblock/NestedCatch.java
@@ -3,8 +3,14 @@ package com.bsl.nested.catchblock;
 public class NestedCatch {
 
 	public static void main(String[] args) {
-		
-		
+
+		try {
+			System.out.println(10 / 0);
+		} catch (Exception e) {
+			String a = null;
+			System.out.println(a.toLowerCase());
+		}
+		System.out.println("main method ended");
 	}
 
 }
