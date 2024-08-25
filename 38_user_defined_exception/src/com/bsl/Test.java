f336bd8c7fccc13bb4e4ff40e1e59a45fc8bbb5b
Kanishka <kanishkab2917@gmail.com>
2024-08-25 12:53:35 +0530

    i can see my exception working perfectly

--- 38_user_defined_exception/src/com/bsl/Test.java
+++ 38_user_defined_exception/src/com/bsl/Test.java
@@ -1,3 +1,30 @@
 package com.bsl;
 
 import java.security.InvalidAlgorithmParameterException;
+
+class InvalidAgeException extends Exception {
+	public InvalidAgeException(String msg) {
+
+		System.out.println(msg);
+	}
+}
+
+public class Test {
+
+	public static void main(String[] args) {
+		try {
+			vote(12);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	public static void vote(int age) throws InvalidAgeException {
+		if (age < 18) {
+			throw new InvalidAgeException("not eligle for voting");
+		} else {
+			System.out.println("eligible for voting");
+		}
+	}
+
+}
