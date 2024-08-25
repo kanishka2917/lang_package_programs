9f843f001f9d2360f979206af562c346eaa765c1
Kanishka <kanishkab2917@gmail.com>
2024-08-25 12:16:05 +0530

    trying to solve all me exception

--- 36_throws_and_Throw/src/com/bsl/throwsandthrow/Test.java
+++ 36_throws_and_Throw/src/com/bsl/throwsandthrow/Test.java
@@ -2,12 +2,18 @@ package com.bsl.throwsandthrow;
 
 public class Test {
 
-	public static void main(String[] args) {
-		void div(int a, int b) {
-			if(b==0) {
-				
-			}
+	void div(int a, int b) {
+		if (b == 0) {
+			throw new ArithmeticException();
+		} else {
+			int c = a / b;
+			System.out.println(c);
 		}
+	}
+
+	public static void main(String[] args) {
+		Test t = new Test();
+		t.div(20, 0);
 	}
 
 }
