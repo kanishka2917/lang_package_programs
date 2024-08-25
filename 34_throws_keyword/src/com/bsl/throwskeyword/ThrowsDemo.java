fd5f5781ce49b068d802cf7e1e3b49328ae1eb79
Kanishka <kanishkab2917@gmail.com>
2024-08-25 12:00:55 +0530

    Getting an interruptedException, trynna solve it

--- 34_throws_keyword/src/com/bsl/throwskeyword/ThrowsDemo.java
+++ 34_throws_keyword/src/com/bsl/throwskeyword/ThrowsDemo.java
@@ -3,7 +3,10 @@ package com.bsl.throwskeyword;
 public class ThrowsDemo {
 
 	public static void main(String[] args) {
-		
-	}
 
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i);
+			Thread.sleep(1000);
+		}
+	}
 }
