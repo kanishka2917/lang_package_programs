9299df9f77ce0ea5f051aadce932831468591faf
Kanishka <kanishkab2917@gmail.com>
2024-08-25 12:26:13 +0530

    Getting an interruptedException, trynna solve it

--- 37_try_catch_vs_Throws/src/com/bsl/Demo.java
+++ 37_try_catch_vs_Throws/src/com/bsl/Demo.java
@@ -1,10 +1,18 @@
 package com.bsl;
 
+import java.util.Iterator;
+
 public class Demo {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void wait() {
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i);
+			Thread.sleep(1000);
 
+		}
 	}
 
+	public static void main(String[] args) {
+		wait();
+	}
 }
