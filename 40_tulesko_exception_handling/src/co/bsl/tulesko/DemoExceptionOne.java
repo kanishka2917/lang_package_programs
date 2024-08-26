36fc0d1067eb16adab0278481e7c32c4932a81c3
Kanishka <kanishkab2917@gmail.com>
2024-08-26 22:25:43 +0530

    solved the exceptions and my output is clear

--- 40_tulesko_exception_handling/src/co/bsl/tulesko/DemoExceptionOne.java
+++ 40_tulesko_exception_handling/src/co/bsl/tulesko/DemoExceptionOne.java
@@ -2,4 +2,17 @@ package co.bsl.tulesko;
 
 public class DemoExceptionOne {
 
+	public static void main(String[] args) {
+
+		int i, j, k = 0;
+		i = 8;
+		j = 0;
+		try {
+			k = i / j;
+		} catch (Exception e) {
+			System.out.println("cannot divide by zero");
+		}
+		System.out.println(k);
+	}
+
 }
