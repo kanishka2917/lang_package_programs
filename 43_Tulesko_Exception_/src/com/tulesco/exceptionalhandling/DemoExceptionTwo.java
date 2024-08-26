18702d0844ce9c02529ad435ac18be88a1939fd1
Kanishka <kanishkab2917@gmail.com>
2024-08-26 22:29:02 +0530

    learning more into exceptions

--- 43_Tulesko_Exception_/src/com/tulesco/exceptionalhandling/DemoExceptionTwo.java
+++ 43_Tulesko_Exception_/src/com/tulesco/exceptionalhandling/DemoExceptionTwo.java
@@ -2,26 +2,3 @@ package com.tulesco.exceptionalhandling;
 
 public class DemoExceptionTwo {
 
-	private static int[] a;
-
-	public static void main(String[] args) {
-
-		int i, j, k = 0;
-		i = 8;
-		j = 2;
-		try {
-			k = i / j;
-			for (int c = 0; c <= 4; c++) {
-				a[c] = c + 1;
-			}
-			for (int value : a) {
-				System.out.println(value);
-			}
-		} catch (Exception e) {
-			System.out.println("cannot divide by zero"+e);
-		}
-		System.out.println(k);
-
-	}
-
-}
