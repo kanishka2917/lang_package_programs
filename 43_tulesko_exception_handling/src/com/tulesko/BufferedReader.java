6db7f66a5d3379189e231324207313dcdcd57066
Kanishka <kanishkab2917@gmail.com>
2024-08-29 21:40:00 +0530

    learning more about exceptions

---
+++ 43_tulesko_exception_handling/src/com/tulesko/BufferedReader.java
@@ -0,0 +1,16 @@
+package com.tulesko;
+
+import java.io.InputStreamReader;
+
+public class BufferedReader {
+
+	public BufferedReader(InputStreamReader inputStreamReader) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String readLine() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- 43_tulesko_exception_handling/src/com/tulesko/DemoExceptionFour.java
+++ 43_tulesko_exception_handling/src/com/tulesko/DemoExceptionFour.java
@@ -1,10 +1,29 @@
 package com.tulesko;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class DemoExceptionFour {
 
 	public static void main(String[] args) {
-		
-		
+
+		int i, j = 1, k = 0;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		i = 8;
+		try {
+			System.out.println("enter a number : ");
+			j = Integer.parseInt(br.readLine());
+			k = i / j;
+			System.out.println("output is " + k);
+			System.out.println("done");
+
+		}
+
+		catch (ArithmeticException e) {
+			System.out.println("cannot divide by zero" + e);
+		} catch (Exception e) {
+			System.out.println("some unknown error");
+		}
 	}
 
 }
