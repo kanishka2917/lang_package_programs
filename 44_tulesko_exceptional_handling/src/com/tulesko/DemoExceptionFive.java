7742fc1d66e1b6a6b2285b563255860f08899d59
Kanishka <kanishkab2917@gmail.com>
2024-08-29 21:50:50 +0530

    the try resource

--- 44_tulesko_exceptional_handling/src/com/tulesko/DemoExceptionFive.java
+++ 44_tulesko_exceptional_handling/src/com/tulesko/DemoExceptionFive.java
@@ -1,9 +1,22 @@
 package com.tulesko;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class DemoExceptionFive {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws Exception {
+
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in)))
+		{
+			String str = "";
+			str = br.readLine();
+		} 
+		catch (Exception e) 
+		{
+			System.out.println(e);
+		}
+
 	}
 
 }
