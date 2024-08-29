6fcb16b963f249a7ec6c683525c21283d42b14fb
Kanishka <kanishkab2917@gmail.com>
2024-08-29 21:10:45 +0530

    add a default catch block, for the exception you are not sure of

--- 42_tulesko_exceptional_handling/src/com/tulesko/DemoExceptionThree.java
+++ 42_tulesko_exceptional_handling/src/com/tulesko/DemoExceptionThree.java
@@ -1,3 +1,31 @@
 package com.tulesko;
 
 public class DemoExceptionThree {
+
+	private static int[] a;
+
+	public static void main(String[] args) {
+
+		int i, j, k = 0;
+		i = 8;
+		j = 2;
+		try {
+			k = i / j;
+			for (int c = 0; c <= 4; c++) {
+				a[c] = c + 1;
+			}
+			for (int value : a) {
+				System.out.println(value);
+			}
+		} catch (ArithmeticException e) {
+			System.out.println("cannot divide by zero" + e);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("maximum number of vslues is 4");
+		} catch (Exception e) {
+			System.out.println("some unknown error");
+		}
+		System.out.println(k);
+
+	}
+
+}
