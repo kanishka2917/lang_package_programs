701a57359faf675d0f7e8a304728210378406d3a
Kanishka <kanishkab2917@gmail.com>
2024-08-29 21:02:33 +0530

    one try and multiple catch methods

--- 41_tulesko_exception_handling/src/com/tulesko/exception/handling/DemoExceptionTwo.java
+++ 41_tulesko_exception_handling/src/com/tulesko/exception/handling/DemoExceptionTwo.java
@@ -17,8 +17,11 @@ public class DemoExceptionTwo {
 			for (int value : a) {
 				System.out.println(value);
 			}
-		} catch (Exception e) {
+		} catch (ArithmeticException e) {
 			System.out.println("cannot divide by zero"+e);
+		}
+		catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("maximum number of vslues is 4");
 		}
 		System.out.println(k);
 
